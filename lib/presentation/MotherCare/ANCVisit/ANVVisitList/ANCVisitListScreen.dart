11d0418437cd823fffdba675d13a98e4e4e631ab
NikhilBhandigare <nikhil.alphonsol@gmail.com>
2025-12-23 11:24:25 +0530

    my change apisdsdsdsfsdsdsasdsdsdsdssdkjfisdss

--- lib/presentation/MotherCare/ANCVisit/ANVVisitList/ANCVisitListScreen.dart
+++ lib/presentation/MotherCare/ANCVisit/ANVVisitList/ANCVisitListScreen.dart
@@ -58,7 +58,7 @@ class _AncvisitlistscreenState extends State<Ancvisitlistscreen> {
       ON r.beneficiary_ref_key = bn.unique_key
     WHERE
       r.rn = 1
-      AND r.mother_care_state = 'anc_due_state'
+      AND r.mother_care_state = 'anc_due'
       AND bn.is_deleted = 0
     ORDER BY r.created_date_time DESC; 
     ''',
