ca6da770bb728e78256bb22f3e32d6f93b18e31d
sanketpandit-dev <sanket.alphonsol@gmail.com>
2026-01-02 11:04:43 +0530

    new 2/1/2025

--- lib/presentation/MotherCare/ANCVisit/ANCVisitForm/bloc/anvvisitform_bloc.dart
+++ lib/presentation/MotherCare/ANCVisit/ANCVisitForm/bloc/anvvisitform_bloc.dart
@@ -358,7 +358,6 @@ class AnvvisitformBloc extends Bloc<AnvvisitformEvent, AnvvisitformState> {
               'is_synced': 0,
               'is_deleted': 0,
             };
-
             print('Inserting mother care activity for pregnant head: ${jsonEncode(motherCareActivityData)}');
             await LocalStorageDao.instance.insertMotherCareActivity(motherCareActivityData);
             print('✅ Successfully inserted mother care activity');
